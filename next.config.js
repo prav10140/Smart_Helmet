059e0a0c1b78bbeb55c941a54d2e7052083dfcb5
PRAVEEN PATEL <prav9406@gmail.com>
2025-08-01 17:15:22 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  // Disable static optimization for dynamic imports
+  output: "standalone",
+}
+
+module.exports = nextConfig
